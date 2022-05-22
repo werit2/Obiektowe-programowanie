df7af2e2940de32d3abf4110502b45b29dd93b04
djkek <64024016+djkek@users.noreply.github.com>
2022-05-22 17:52:26 +0200

    Przełączanie działa

--- app/src/main/java/com/example/zoo/KatalogActivity.kt
+++ app/src/main/java/com/example/zoo/KatalogActivity.kt
@@ -1,25 +1,19 @@
 package com.example.zoo
 
-// MainActivity to mapka próbowałem zmienić nazwe i błedy najebało -DJ
-
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class KatalogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_katalog)
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener{
-            val intent = Intent(this@MainActivity, KatalogActivity::class.java)
+            val intent = Intent(this@KatalogActivity, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
-
-
 }
-
